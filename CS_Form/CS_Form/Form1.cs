341d294412c5a8c05a582b774d90a82cad88e5d7
MSI\hisui <zeal2460060@fsgci.jp>
2024-11-01 16:02:23 +0900

    課題を終わらせた

---
+++ CS_Form/CS_Form/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+            for (int cnt = 0; cnt < 10; cnt++)
+            {
+                
+                Testbutton testbutton = new Testbutton((cnt% 3) * 100,(cnt/3) * 100, 100, 100,cnt);
+                Controls.Add(testbutton);
+            }
+        }
+    }
+}
---
+++ CS_Form/CS_Form/Testbutton.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+
+namespace CS_Form
+{
+    internal class Testbutton : Button
+
+        /// <samary>
+        /// コンストラクタ
+        /// クラスを生成したときに呼び出される
+        /// </samary>
+    {
+        public Testbutton(int x, int y,int width,int height,int ID)
+        {
+            // クリックイベントにOneclick関数を登録
+            // ボタンをクリックしたときに登録した関数を実行させます
+            Click += Oneclick;
+
+            // ボタン内に文字を表示させる
+            Text = "(=^・・^=)"+ID.ToString();
+
+            Console.Write(Text);
+
+            Location = new Point(x,y);
+
+            Size = new Size(width,height);
+
+
+        }
+
+        public void Oneclick(object sender, EventArgs s)
+        {
+            MessageBox.Show(Text);
+        }
+    }
+}
